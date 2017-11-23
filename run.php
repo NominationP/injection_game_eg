5f020e4384f03ce68daf9c8b2036a18152ba094b
NominationP <zhangze1029@gmail.com>
2017-11-23 13:49:44 +0800

    v2.0 finish

--- run.php
+++ run.php
@@ -6,15 +6,6 @@ require_once 'woodenSword.php';
 require_once 'magicSword.php';
 require_once 'monster.php';
 
-// $weapon = new Weapon("木剑",20,20);
-// $role = new Role("yibo",$weapon);
-// $monsterBoss = new Monster("boss",100,3);
-// $monsterA = new Monster("monsterA",50,1);
-// $monster = [$monsterA,$monsterBoss];
-// $role->weapon->attack($monster,$weapon);
-
-// $role = new Role("yibo",$weapon);
-
 $wooden = new WoodenSword;
 $magic = new MagicSword;
 $role = new Role("yobo",$wooden);
